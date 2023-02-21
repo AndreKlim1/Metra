826a587ea55d0b838f04e9c09a1d149d9eb40e22
AndreKlim1 <andreyklimovich159@gmail.com>
2023-02-21 19:16:45 +0300

    Добавьте файлы проекта.

---
+++ Metr1/Holsted.h
@@ -0,0 +1,68 @@
+#pragma once
+
+namespace Metr1 {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// —водка дл€ Holsted
+	/// </summary>
+	public ref class Holsted : public System::Windows::Forms::Form
+	{
+	public:
+		Holsted(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: добавьте код конструктора
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// ќсвободить все используемые ресурсы.
+		/// </summary>
+		~Holsted()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+
+	private:
+		/// <summary>
+		/// ќб€зательна€ переменна€ конструктора.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// “ребуемый метод дл€ поддержки конструктора Ч не измен€йте 
+		/// содержимое этого метода с помощью редактора кода.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->SuspendLayout();
+			// 
+			// Holsted
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(284, 261);
+			this->Name = L"Holsted";
+			this->Text = L"Holsted";
+			this->Load += gcnew System::EventHandler(this, &Holsted::Holsted_Load);
+			this->ResumeLayout(false);
+
+		}
+#pragma endregion
+	private: System::Void Holsted_Load(System::Object^ sender, System::EventArgs^ e) {
+	}
+	};
+}
