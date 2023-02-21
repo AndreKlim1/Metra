d83f7e06657833936fe79917ad83a2b066fd856b
Nikmoofte <ilya.vailov@gmail.com>
2023-02-22 00:30:44 +0300

    Added basic backbone

--- Metr1/BackBone.cpp
+++ Metr1/BackBone.cpp
@@ -3,10 +3,72 @@
 #include <string>
 #include <map>
 
-std::vector<std::string> Tags = {"pust", "[", "]", "."};
+std::map<std::string, size_t> operators = {{"puts", 0}, {"[", 0}, {"]", 0}, {".", 0}, {" ", 0}, {"\n", 0}, 
+                                           {"\"", 0}, {"+", 0}, {"-", 0}, {"*", 0}, {"/", 0}, {",", 0}, 
+                                           {"(", 0}, {")", 0}, {"for", 0}, {"loop", 0}, {"while", 0}, {"do", 0}, {"end", 0}, {"in", 0}};
+std::map<std::string, size_t> operands;
 
-int GetInfo()
+
+void GetInfo(std::string code)
 {
-    
-    return 0;
+    std::string prevToken = "", currToken = "";
+
+    auto mapEnd = operators.end();
+    for(auto symbol : code)
+    {
+        std::map<std::string, size_t>::iterator  iter; 
+        if((iter = operators.find(std::string(1, symbol))) != mapEnd)
+        {
+            (*iter).second++;
+
+            if(symbol == '=')
+
+ 
+            if(prevToken == ".")
+                operators[currToken]++;
+            else
+                operands[currToken]++;
+            currToken.clear();
+            prevToken = (*iter).first;
+            continue;
+        }
+
+        currToken.push_back(symbol);
+        if((iter = operators.find(currToken)) != mapEnd)
+        {
+            (*iter).second++;
+            currToken.clear();
+        }
+    } 
+    std::map<std::string, size_t>::iterator  iter;  
+    if((iter = operators.find(currToken)) != mapEnd)
+    {
+        (*iter).second++;
+        currToken.clear();
+    } 
+    else
+    {
+        if(prevToken == ".")
+            operators[currToken]++;
+        else
+            operands[currToken]++;
+        currToken.clear();
+    }
+
+    std::cout << "Operators:\n";
+    for(auto elem : operators)
+    {
+        std::cout << elem.first << ": " << elem.second << '\n';
+    }
+    std::cout << std::endl;
+
+    std::cout << "Operands:\n";
+    for(auto elem : operands)
+    {
+        std::cout << elem.first << ": " << elem.second << '\n';
+    }
+
+
+
+    return;
 }
